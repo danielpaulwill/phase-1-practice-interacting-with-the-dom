a3828b302123690169293eeb0f77bd70b6023c17
Daniel Williams <danielpaulwill@gmail.com>
2021-08-23 15:58:54 -0400

    Finally finished

--- js/challenge.js
+++ js/challenge.js
@@ -1,6 +1,10 @@
 
 let i = 0
-
+let plusButton = document.querySelector('#plus')
+let minusButton = document.querySelector('#minus')
+let likeButton = document.querySelector('#heart')
+let pauseButton = document.getElementById('pause');
+let submitButton = document.querySelector('#submit')
 
 //Increment the counter one number every second
 let counter = document.querySelector('#counter');
@@ -13,59 +17,86 @@ let intervalCounter = setInterval(increment, 1000);
 
 
 //Manually increment the counter with the + button
-let plusButton = document.querySelector('#plus').addEventListener('click', (e => {
+plusButton.addEventListener('click', (e => {
     let buttonTarget = e.target
     buttonTarget = counter.innerText = i++
 }));
 
 
 //Manually decrement the counter with the - button
-let minusButton = document.querySelector('#minus').addEventListener('click', (e => {
+minusButton.addEventListener('click', (e => {
     let buttonTarget = e.target
     buttonTarget = counter.innerText = i--
 }));
 
 
-//Like a number on the counter
-let likeClick = document.querySelector('#heart').addEventListener('click', e => {
-  likeClickIncrementer()
+//As a user, I can 'like' an individual number of the counter. I should see the count of the number of 'likes' associated with that number displayed.
+let likeClickQuantity = 0
+
+let likeClickCounter = setInterval(resetLikeClickQuantity, 1000);
+
+likeButton.addEventListener('click', e => {
   commentLikes()
 });
 
-let likeClickQuantity = 1
-
 function commentLikes() {
-  let likeComment = document.querySelector('.Comments') //Selected the comments area on the page
+  let likeComment = document.querySelector('.likes') //Selected the comments area on the page
+  if (document.getElementById(`${i}`)){
+     document.getElementById(`${i}`).innerText = (`${i} has been liked ${likeClickIncrementer()} times`);
+  } else {
   let likeList = document.createElement('li');    //Create the list element
-  let listText = document.createTextNode(`${i} has been liked ${likeClickQuantity} times`) //Create the text
+  likeList.id = i
+  let listText = document.createTextNode(`${i} has been liked ${likeClickIncrementer()} times`) //Create the text
   likeList.appendChild(listText);   //Push text into list
   likeComment.appendChild(likeList); //Push list into comments area
+}};
+
+function resetLikeClickQuantity() {
+  return likeClickQuantity = 0
 };
-
-
-
 
 function likeClickIncrementer() {
-  document.querySelector('#heart').addEventListener('click', e => {
-    likeClickQuantity = ++likeClickQuantity
-//    console.log(likeClickQuantity)
-  })
+    likeClickQuantity++
+    return likeClickQuantity  
 };
-
-//I should see the count of the number of 'likes' associated with that number displayed.
-//
-//Click the like button, only append one comment string per second, increment the like count per click
-
-
 
 
 // As a user, I can pause the counter, which should:
-
-// pause the counter
 // disable all buttons except the pause button
-// switch the label on the button from "pause" to "resume"
+pause.addEventListener('click', e => {
+  if (pauseButton.innerText === 'pause') {
+  clearInterval(intervalCounter)   // pause the counter
+  plusButton.disabled = true   // disable all buttons except the pause button
+  minusButton.disabled = true
+  likeButton.disabled = true
+  submitButton.disabled = true
+  pauseButton.innerText = 'resume'   // switch the label on the button from "pause" to "resume"
+} else {
+  intervalCounter = setInterval(increment, 1000);
+  plusButton.disabled = false
+  minusButton.disabled = false
+  likeButton.disabled = false
+  submitButton.disabled = false
+  pauseButton.innerText = 'pause'
+}
+});
+
+
 // As a user, I should be able to click the "restart" button to restart the counter and re-enable the buttons.
 
 // As a user, I can leave comments on my gameplay, such as: "Wow, what a fun game this is."
+let form = document.querySelector('form')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  //When submit button is pressed
+  let formInput = document.getElementById('comment-input').value
+  let commentInput = document.createTextNode(`${formInput}`)   //Actual text entered
+  let submittedComment = document.querySelector('.Comments') //*selected - Comments area on the DOM
+  let commentParagraph = document.createElement("p")   //Create a new <p>
+  let commentText = document.createTextNode(commentInput)   //Put input text into a text node
+  commentParagraph.appendChild(commentInput)   //Push commentText into commentParagraph
+  submittedComment.appendChild(commentParagraph)
+  });
 
 
